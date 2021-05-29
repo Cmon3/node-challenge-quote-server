e431c41128719293417e5dcd8e50830adfcd000f
Cmon3 <cmonlg@gmail.com>
2021-05-29 18:15:40 +0200

    First commit homework

--- server.js
+++ server.js
@@ -11,13 +11,41 @@ const quotes = require("./quotes.json");
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
+app.get("/", function (request, response) {
+  response.send("Hello world")
+})
 //   /quotes            - Should return all quotes (json)
+app.get("/quotes", function (request, response) {
+  response.send(200, `These are all your quotes: ${JSON.stringify(quotes)}`);
+})
 //   /quotes/random     - Should return ONE quote (json)
+app.get("/quotes/random", function (request, response) {
+  const randomQuote = pickFromArray(quotes);
+  response.send(randomQuote);
+});
+
 app.get("/", function (request, response) {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
+
+app.get("/quotes/search", function (request, response) {
+  let term = request.query.term.toLowerCase();
+  let matches = findQuotesContainingTerm(quotes, term)
+  response.send(matches);
+});
+
+function findQuotesContainingTerm(q, t) {
+  let matches = []
+  q.forEach(item => {
+    if (item.quote.includes(t) || item.author.includes(t)) {
+      matches.push(item)
+    }
+  })
+  return matches;
+};
+
 
 //...END OF YOUR CODE
 
